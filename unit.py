7e72093d775b604c12e5c5d0adb59a28ce61ab3b
NIKOLAOS BOUMAKIS <programmesbynboumakis@gmail.com>
2024-04-10 13:36:16 +0300

    Formatting

--- unit.py
+++ unit.py
@@ -294,7 +294,7 @@ async def recv_server(socket: WebSocketClientProtocol,
             await button_led_queue.put((i, message))
             await matrix_queue.put((i, message))
             await sound_queue.put((i, message))
-        i+=1
+        i += 1
 
 
 async def send_server(socket: WebSocketClientProtocol, message: bytes):
@@ -343,7 +343,7 @@ async def main(args: list[str]):
     ''' The main function for the unit '''
 
     options = parse_arguments(args)
-    i =0
+    i = 0
     # Initialize the hardware interface
     button: Button = Button(26)
     button_led: RGBLED = RGBLED(17, 27, 22)
@@ -416,9 +416,9 @@ async def main(args: list[str]):
             await asyncio.sleep(RECHECK_INTERVAL)
 
             stop_blink = {'type': 'BUTTON_LED', 'value': 'STOP'}
-            
+
             await button_led_queue.put((i, stop_blink))
-            i+=1
+            i += 1
 
 if __name__ == "__main__":
     asyncio.run(main(sys.argv[1:]))
